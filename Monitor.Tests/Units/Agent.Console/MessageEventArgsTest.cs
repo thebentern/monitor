52956b4b92448e83f7781ca49129b11507080f29
Benjamin Meadors <ben.meadors@seaseducation.com>
2016-04-27 16:57:53 -0500

    Tests for message args

---
+++ Monitor.Tests/Units/Agent.Console/MessageEventArgsTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+using Fclp;
+using FluentAssertions;
+using Monitor.Agent.Console;
+
+namespace Monitor.Tests.Units.Agent.Console
+{
+    public class MessageEventArgsTest
+    {
+        [Test]
+        public void Throw_Exception_When_Message_Is_Missing() =>
+           Assert.Throws<ArgumentNullException>(() => new MessageEventArgs(null));
+    }
+}
